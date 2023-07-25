e0e1d165c510449043b0d304ad24b291156f0a2c
Joao Pedro <joao21dev@gmail.com>
2023-07-25 02:44:07 -0300

    fastify and ts added

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
+
+app
+  .listen({
+    port: 3000,
+  })
+  .then(() => {
+    console.log(`HTTP server running`);
+  });
